a0fe2e8d6a53036a856dcbe32031d0effa2d20da
louisefleischer <lfleisch@stanford.edu>
2017-11-13 13:04:41 -0800

    File + lander
    Set abstract files in one rep and added movement functions to the lander

--- lander.jl
+++ lander.jl
@@ -1,1 +1,14 @@
 #Lander
+Type Lander
+    pos::Vector{Float64}
+    vel::Vector{Float64}
+end
+
+function move(lander::Lander,t)
+    lander.pos += lander.vel*t
+end
+
+function impulse(lander::Lander,delta_v)
+    lander.vel+=delta_v
+end
+
