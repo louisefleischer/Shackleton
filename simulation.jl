b90bbef6f5ace7818e01517199a52ef4fe80a465
pierrelachevre18 <lpierre@stanford.edu>
2017-12-06 22:45:50 -0800

    Added next state and reward functions

--- simulation.jl
+++ simulation.jl
@@ -14,7 +14,7 @@ end
 
 MAP_SIZE = 100
 true_map = vcat([1, 2, 3, 3, 3, 4, 5, 6, 6, 5],
-    collect(linspace(1,50,(100-38))),
+    collect(ceil.(linspace(1,50,(100-38)))),
     [4, 3, 2, 1, 1, 1, 2, 3, 4, 5, 6, 5, 4, 6, 10, 11, 15, 22, 30, 30, 32, 31, 25, 20, 15, 1,1, 1])
 
 #true_map = collect(linspace(1,50,100))
@@ -34,28 +34,6 @@ function update_belief(observation_map,old_observations, new_observation, belief
     # updates the belief_map based on the new observations made
     # aferwards it will be probability map 
 
-<<<<<<< HEAD
-    belief_map[new_observation[:,1],:]=[new_observation[:,2],1]
-
-
-    # for i in 1:length(observation_map)
-    #     if observation_map[i]>-1
-    #         belief_map[i,:]=[observation_map[i],1]
-
-    #     else
-    #         # find the closest observation point and take that value
-    #         j = 1
-    #         while j+i<MAP_SIZE&i-j>MAP_SIZE
-    #             if observation_map[i-j]!=-1
-    #                 belief_map[i,1]=observation_map[i-j]
-
-    #         end
-    #         belief_map[i,1]= observation_map[j]    
-            
-    #     end
-    # end
-    return belief_map
-=======
     observation_point = 0
     for i in 1:length(observation_map)
         if observation_map[i]>-1
@@ -75,7 +53,6 @@ function update_belief(observation_map,old_observations, new_observation, belief
         end
         return belief_map
     end
->>>>>>> 838866cb5aaf708c716872129cac2bd893eb88d0
 end
 
 function find_flat(belief_map)
@@ -95,15 +72,42 @@ function make_observation(true_map, lander)
     o = Observation(x,h)
 end
 
-function compute_reward(obs,lander,action)
-    # backpropagation of rewards
+function next_state(x,z,action)
+    # compute the next state
+    # input: state coordinates, action
+    return [x-2+action;z-1]
 end
+
+function compute_reward(x,z,obs,lander,action,observation_map)
+    # compute reward based on potential observation and action
+    # set constants
+    R_thrust=-.5
+    R_newobs=10
+    # Cost of action
+    R_action=R_thrust(2-R_thrust*(action==2))
+    # Reward for observation
+    R_obs=0
+    if z==lander.z-3
+        x = [lander.x-div(lander.z,2); lander.x; lander.x + div(lander.z,2)]
+        R_obs=R_newobs*length(findin(observation_map(x),-1))
+    end
+    return R_obs+R_action
+end
+
 function U_ground(belief_map,x)
     # Calculate utility of potential landing site
+    U_valid=200
+    U_invalid=-50
+    U_maybe_3=100
+    U_maybe_2=50
+    U_maybe_1=0
 end
+
 function update_utility(belief_map,lander)
     #Update utility map from bottom to top
+    U_crash=-200
 end
+
 function take_action(flat_place, lander)
     # returns the next action
     #find closest one
@@ -114,20 +118,7 @@ end
 
 iteration = 0
 while lander.z>(true_map[lander.x])
-
-<<<<<<< HEAD
-if iteration%3=0
-# observe
-o = make_observation(true_map, lander)
-observation_map[o.x] = o.h
-new_observation = [o.x,o.h]
-
-# update your belief
-belief_map = update_belief(observation_map, old_observations, new_observation, belief_map)
-belief_map = true_map
-old_observations = vcat(old_observations;new_observation)
-end
-=======
+    if iteration%3==0
     # observe
     o = make_observation(true_map, lander)
     observation_map[o.x] = o.h
@@ -135,23 +126,13 @@ end
     # update your belief
     belief_map = update_belief(observation_map, belief_map)
     belief_map = true_map
->>>>>>> 838866cb5aaf708c716872129cac2bd893eb88d0
 
     # find flat parts in the belief map 
     flat = find_flat(belief_map)
-
-<<<<<<< HEAD
-# make your decision
-lander.x = lander.x + take_action(flat, lander)
-
-
-
-lander.z -= 1
-=======
+    end
     # make your decision
     lander.x = lander.x + take_action(flat, lander)
     lander.z -= 1
->>>>>>> 838866cb5aaf708c716872129cac2bd893eb88d0
 
     # keep in memory for plotting
     x_path = hcat(x_path,[lander.x])
